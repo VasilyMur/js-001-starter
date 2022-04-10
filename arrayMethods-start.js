6a9dea8e67e683421d06c5465d14d1013ff507d6
vasilymur <vmouravi@gmail.com>
2022-04-10 18:32:32 +0300

    init

---
+++ arrayMethods-start.js
@@ -0,0 +1,55 @@
+const fruits = ['apple', 'orange', 'banana', 'kiwi', 'lemon', 'apple', 'banana'];
+
+const juice = ['mixed', 'orange', 'berries'];
+
+const fish = {
+  salmon: 12,
+  seaBass: 8,
+  tuna: 3
+};
+
+const cars = [
+  {
+    id: '22a',
+    data: {
+      brand: 'toyota',
+      model: 'corolla', 
+      produced: 'June 7, 2015'
+    }
+  },
+  {
+    id: '22a',
+    data: {
+      brand: 'BMW',
+      model: 'X3', 
+      produced: 'November 20, 2018'
+    }
+  },
+  {
+    id: 'qq3',
+    data: {
+      brand: 'tesla',
+      model: 'model x',
+      produced: 'January 1, 2022'
+    } 
+  },
+  {
+    id: '54i',
+    data: {
+      brand: 'hyundai',
+      model: 'solaris',
+      produced: 'October 5, 2020'
+    }
+  },
+  {
+    id: 'aa22',
+    data: {
+      brand: 'nissan',
+      model: 'micra',
+      produced: 'September 17, 2013'
+    }
+  },
+];
+
+const bills = [200, 430, 1000, 30, 705, 125, 1330, 510];
+
---
+++ arrays-start.js
@@ -0,0 +1,8 @@
+const students = ['vasily', 'stas', 'mary', 'alex'];
+
+const comments = [
+ {text: 'hello my friend!', id: '111'},
+ {text: 'welcome:)', id: '222'},
+ {text: 'BAD COMMENT', id: '333'},
+ {text: 'glad to see you!', id: '444'},
+];
